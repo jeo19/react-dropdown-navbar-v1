49eaf99a155dfaf6ab41397bb53cd4077b67a1c8
jeo19 <terrakkk@outlook.com>
2022-07-07 21:36:26 +0800

    feat:Add the navbar menu elements and style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,9 @@ import Dropdown from "./Dropdown";
 
 const Navbar = () => {
   const [click, setClick] = useState(false);
+  const [dropdown, setDropdown] = useState(false);
   const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
   return (
     <>
       <nav className="navbar">
@@ -16,6 +18,38 @@ const Navbar = () => {
         <div className="menu-icon" onClick={handleClick}>
           <i className={click ? "fas fa-times" : "fas fa-bars"} />
         </div>
+        <ul className={click ? "nav-menu active" : "nav-menu"}>
+          <li className="nav-item">
+            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+              Home
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/services"
+              className="nav-links"
+              onClick={closeMobileMenu}
+            >
+              Services <i className="fa-solid fa-caret-down"></i>
+            </Link>
+            {dropdown && <Dropdown />}
+          </li>
+          <li className="nav-item">
+            <Link
+              to="/contact-us"
+              className="nav-links"
+              onClick={closeMobileMenu}
+            >
+              Contact Us
+            </Link>
+          </li>
+          <li className="nav-item">
+            <Link to="/signup" className="nav-links" onClick={closeMobileMenu}>
+              Sign Up
+            </Link>
+          </li>
+        </ul>
+        <Button />
       </nav>
     </>
   );
