552c3ed28188e3be0853902ec95bb894ffb4a470
jeo19 <terrakkk@outlook.com>
2022-07-08 21:21:49 +0800

    feat:Add the menuitems data

---
+++ src/components/Dropdown.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import { MenuItems } from "./MenuItems";
+import { Link } from "react-router-dom";
+import "./Dropdown.css";
+
+const Dropdown = () => {
+  return (
+    <>
+      <div></div>
+    </>
+  );
+};
+export default Dropdown;
